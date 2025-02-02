ba4d97f20ac34c68c0b3ff7326e3aa2b1469b703
zsbahtiar <bekerz18@gmail.com>
2025-02-02 14:53:40 +0700

    feat: add config management

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Database struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		Name     string
+	}
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	config.Database.Host = viper.GetString("DB_HOST")
+	config.Database.Port = viper.GetString("DB_PORT")
+	config.Database.User = viper.GetString("DB_USER")
+	config.Database.Password = viper.GetString("DB_PASSWORD")
+	config.Database.Name = viper.GetString("DB_NAME")
+
+	return &config, nil
+}
